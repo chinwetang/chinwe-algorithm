213a2c2720c5c8e26bce5df8703376d9cefff6e3
DESKTOP-PK4J594\tangqiwei <chinwe.tang@qq.com>
2019-04-26 20:17:28 +0800

    加倆

---
+++ lib-java/src/main/java/oa/bluemoon/com/cn/lib_java/_007_Reverse_Integer.java
@@ -0,0 +1,39 @@
+package oa.bluemoon.com.cn.lib_java;
+
+/**
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 123
+ * 输出: 321
+ * 示例 2:
+ * <p>
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ * <p>
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ * <p>
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ */
+public class _007_Reverse_Integer {
+    public static void main(String[] args) {
+        System.out.print(new _007_Reverse_Integer().reverse(-123) + "");
+    }
+
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int y = x % 10;
+            result = result * 10 + y;
+            if (result % 10 != y) {
+                return 0;
+            }
+            x = x / 10;
+        }
+        return result;
+    }
+}
---
+++ lib-java/src/main/java/oa/bluemoon/com/cn/lib_java/_009_Palindrome_Number.java
@@ -0,0 +1,43 @@
+package oa.bluemoon.com.cn.lib_java;
+
+/**
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ * <p>
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ * <p>
+ * 你能不将整数转为字符串来解决这个问题吗？
+ */
+public class _009_Palindrome_Number {
+
+    public static void main(String[] args) {
+        System.out.print(new _009_Palindrome_Number().isPalindrome(1) + "");
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+        int y = 0;
+        while (true) {
+            y = y * 10 + x % 10;
+            x = x / 10;
+            if (x == y)
+                return true;
+            if (y > x)
+                return y / 10 == x;
+        }
+    }
+}
